f30b6bad93056590ce12f5136f6811bfc15146c7
EstebanHen <stebanhenao@gmail.com>
2024-10-31 20:18:25 -0700

    Implementación de controlador y repository

--- src/main/java/com/estraMyPime/backend/Controller/ProfesorController.java
+++ src/main/java/com/estraMyPime/backend/Controller/ProfesorController.java
@@ -1,4 +1,40 @@
 package com.estraMyPime.backend.Controller;
 
+import com.estraMyPime.backend.Model.Profesor;
+import com.estraMyPime.backend.Service.ProfesorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/teachers")
+@CrossOrigin(origins = "*") // Configura CORS
 public class ProfesorController {
+
+    @Autowired
+    private ProfesorService profesorService;
+
+    // Buscar profesor por ID o email
+    @GetMapping
+    public Optional<Profesor> searchProfesor(@RequestParam(required = false) Long id,
+                                             @RequestParam(required = false) String email) {
+        return profesorService.getProfesorByIdOrEmail(id, email);
+    }
+
+    // Agregar o actualizar un profesor en el sistema
+    @PostMapping
+    public Profesor addProfesor(@RequestBody Profesor profesor) {
+        return profesorService.saveProfesor(profesor);
+    }
+
+    // Actualizar campo profesorParteProyecto
+    @PatchMapping("/{id}")
+    public Profesor updateProfesorParteProyecto(@PathVariable Long id,
+                                                @RequestBody Profesor update) {
+        return profesorService.updateProfesorParteProyecto(id, update.getprofesorParteProyecto());
+    }
 }
--- src/main/java/com/estraMyPime/backend/repository/ProfesorRepository.java
+++ src/main/java/com/estraMyPime/backend/repository/ProfesorRepository.java
@@ -1,4 +1,10 @@
 package com.estraMyPime.backend.repository;
 
-public interface ProfesorRepository {
+import com.estraMyPime.backend.Model.Profesor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
+    Optional<Profesor> findByEmail(String email);
 }
